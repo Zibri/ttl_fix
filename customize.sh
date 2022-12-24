11f072769ba5f9abc4ec86c7e172fa7c04c29ff4
Zibri <Zibri@users.noreply.github.com>
2022-12-24 17:42:28 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@ err() {
 
 [[ ! -f "$MAGISKBOOT" ]] && err "Failed to find magiskboot binary. Exiting."
 
-
+rm -rf /data/local/tmp/ttl_fix
 SLOT=`getprop ro.boot.slot_suffix`
 BOOT="boot$SLOT"
 BOOTIMG="/dev/block/by-name/$BOOT"
@@ -21,11 +21,10 @@ cd /data/local/tmp/ttl_fix
 
 dd if="$BOOTIMG" of="/data/local/tmp/ttl_fix/boot.img" conv=notrunc
 $MAGISKBOOT unpack -h "/data/local/tmp/ttl_fix/boot.img"
-( $MAGISKBOOT hexpatch kernel C9220039C816007968F24039E8002836 1F2003D51F2003D568F24039E8002836 ||
+( $MAGISKBOOT hexpatch kernel C9220039C816007968F24039E8002836 1F2003D51F2003D568F24039E8002836 || # Redmi Note 10 pro
 $MAGISKBOOT hexpatch kernel A0160079A022403900040051A0220039 1F2003D5A0224039000400511F2003D5 || # REVVL 6 Pro
 $MAGISKBOOT hexpatch kernel 08050011C9220039C8160079687A4079 080500111F2003D51F2003D5687A4079 # pixel6a
-)
-&& (
+) && (
 $MAGISKBOOT repack "/data/local/tmp/ttl_fix/boot.img" new.img
 dd if=new.img of="$BOOTIMG" conv=notrunc
 mv /data/local/tmp/ttl_fix/boot.img /sdcard/boot.img.backup
