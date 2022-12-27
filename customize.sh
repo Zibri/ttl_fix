1a13196c510d9e1eee8116d4a84031480ed2945c
Zibri <Zibri@users.noreply.github.com>
2022-12-27 23:24:39 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -23,12 +23,13 @@ dd if="$BOOTIMG" of="/data/local/tmp/ttl_fix/boot.img" conv=notrunc
 $MAGISKBOOT unpack -h "/data/local/tmp/ttl_fix/boot.img"
 ( $MAGISKBOOT hexpatch kernel C9220039C816007968F24039E8002836 1F2003D51F2003D568F24039E8002836 || # Redmi Note 10 pro
 $MAGISKBOOT hexpatch kernel A0160079A022403900040051A0220039 1F2003D5A0224039000400511F2003D5 || # REVVL 6 Pro
-$MAGISKBOOT hexpatch kernel 08050011C9220039C8160079687A4079 080500111F2003D51F2003D5687A4079 # pixel6a
+$MAGISKBOOT hexpatch kernel 08050011C9220039C8160079687A4079 080500111F2003D51F2003D5687A4079 || # pixel6a
+$MAGISKBOOT hexpatch kernel C8160079C922003968F24039 1F2003D51F2003D568F24039 # Asus Rog 6
 ) && (
 $MAGISKBOOT repack "/data/local/tmp/ttl_fix/boot.img" new.img
 dd if=new.img of="$BOOTIMG" conv=notrunc
 mv /data/local/tmp/ttl_fix/boot.img /sdcard/boot.img.backup
 rm -rf /data/local/tmp/ttl_fix
-echo -e "\e[94m[!] $@\e[39mPatch removed."
+echo -e "\e[94m[!] $@\e[39mPatch installed."
 sync;sleep 5;reboot
-) || err "Patch not removed."
+) || err "Patch not installed."
